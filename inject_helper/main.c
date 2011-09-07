448429d112763674ede4d6b371b202bc18c373bb
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-09-07 17:11:32 +0200

    inject_helper: the mach_inject_bundle call

---
+++ inject_helper/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  inject_helper
+//
+//  Created by Albert Zeyer on 07.09.11.
+//  Copyright 2011 Albert Zeyer. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <mach_inject_bundle/mach_inject_bundle.h>
+#include "SIMBL.h"
+
+int main (int argc, const char * argv[])
+{
+	assert(argc == 2);
+	pid_t pid = atoi(argv[1]);
+	assert(pid > 0);
+	
+	fprintf(stderr, "injecting into %i ...\n", pid);
+	mach_inject_bundle_pid(SIMBLE_bundle_path, pid);	
+    return 0;
+}
